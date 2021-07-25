039e9c5a1875ba653f9c6262ff086f1569e37062
anargiris <anargiriss@hotmail.com>
2021-07-25 15:46:56 +0300

    responsiveness

--- components/BrandSection.js
+++ components/BrandSection.js
@@ -17,50 +17,50 @@ const BrandSection = () => {
         />
       </div>
       <div className="flex-1 font-cab px-1 py-4 md:py-0 text-gray-darkest">
-        <h3 className="font-next text-3xl mb-4 text-blue-600">
+        <h3 className="font-next text-3xl 2xl:text-5xl mb-4 text-blue-600">
           I create modern websites.
         </h3>
-        <p className="mb-2">
+        <p className="mb-2 2xl:text-2xl">
           I want to help small businesses and individuals get their own website.
           People trust websites more than, let's say, Instagram or Facebook
           profiles. It gives you more control over your image. Think of it as
           building your own tribe.
         </p>
-        <p>
+        <p className="2xl:text-2xl">
           Showing your work, contact information, pricing and more helps users
           understand what your business is all about and connect with it.
         </p>
         <div className="py-5">
-          <h4 className="font-next text-2xl mb-4 text-blue-800">
+          <h4 className="font-next text-2xl 2xl:text-4xl mb-4 text-blue-800">
             Building a website NOW can help you LATER.
           </h4>
-          <p className="mb-2">
+          <p className="mb-2 2xl:text-xl">
             It's a long term investment. Growing your online traffic will help
             evolve your brand faster. Over time, you'll be getting a huge
             advantage and your future projects/products will reach a wider
             audience.
           </p>
-          <p>
+          <p className="2xl:text-xl">
             Having the control over what information you would like to share is
             powerful.
           </p>
         </div>
         <div>
-          <h5 className="font-next text-2xl mb-2 text-blue-800">
+          <h5 className="font-next text-2xl 2xl:text-4xl mb-2 text-blue-800">
             Online visibility.
           </h5>
-          <p className="mb-2">
+          <p className="mb-2 2xl:text-xl">
             People search online for things. Being searchable online is the
             leading factor of building strong brand credibility.
           </p>
-          <p>
+          <p className="2xl:text-xl">
             Imagine two local coffee selling businesses but only one shows up on
             Google..
           </p>
         </div>
-        <p className="mt-10 text-gray-darkest">
+        <p className="mt-10 text-gray-darkest 2xl:text-2xl">
           <Link href="/work">
-            <span className="cursor-pointer text-yellow-500 text-lg hover:text-yellow-700 transitoin duration-200">
+            <span className="cursor-pointer 2xl:text-2xl text-yellow-500 text-lg hover:text-yellow-700 transitoin duration-200">
               See my work
             </span>
           </Link>
