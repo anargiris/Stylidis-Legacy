f53847323d179c712a3529d026fc547b6e97e52b
anargiris <anargiriss@hotmail.com>
2021-08-01 19:47:39 +0300

    gsc verification

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,19 @@ import Footer from "../components/Footer";
 import HomePageHeader from "../components/HomePageHeader";
 import OfferSection from "../components/OfferSection";
 import BrandSection from "../components/BrandSection";
-import Head from "../components/PageHead";
+import PageHead from "../components/PageHead";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Head
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="mlWJ3t_q2M8Vg7oJinpYE8_0MvQoc4qQI2EiJFvgnRo"
+        />
+      </Head>
+      <PageHead
         title="Anargyros Stylidis - Web Developer - Greece"
         description="Web developer from Greece specializing in creating modern, elegant, responsive websites. Contact for more information or if you need your own website."
       />
