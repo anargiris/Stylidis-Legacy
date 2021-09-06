c45f2ac53d8c8bef99a2c83f99d9f6712054f9a9
anargiris <anargiriss@hotmail.com>
2021-09-06 16:24:53 +0300

    hero changes

--- components/HomePageHeader.js
+++ components/HomePageHeader.js
@@ -7,7 +7,7 @@ import MobileNav from "./MobileNav";
 
 const HomePageHeader = () => {
   return (
-    <div className="md:min-h-screen main-header relative pb-10 md:pb-0">
+    <div className="md:h-screen main-header relative pb-10 md:pb-0">
       <Navbar />
       <header>
         <div className={`bg-gray-darkest md:p-6 p-4 z-40 relative`}>
@@ -15,9 +15,6 @@ const HomePageHeader = () => {
             <h1 className="text-4xl md:text-5xl 2xl:text-7xl lg:px-20 md:px-10 text-gray-200 font-bold py-2 font-next">
               Anargyros Stylidis
             </h1>
-            <span className="text-gray-100 cursor-default font-bold md:text-5xl text-2xl  font-cab absolute bottom-0 right-0 opacity-20 tracking-wide">
-              I create websites.
-            </span>
           </div>
         </div>
       </header>
@@ -27,10 +24,8 @@ const HomePageHeader = () => {
             I'll build your{" "}
             <span className={styles.highlighted}>online presence.</span>
           </span>
-          <div
-            className={`${styles.borderstuff}  border-b-2 border-pink-border pb-1`}
-          >
-            <h2 className="md:w-1/2 md:mt-16 mt-8 font-cab text-lg 2xl:text-3xl text-gray-800">
+          <div className={`pb-1`}>
+            <h2 className="md:w-1/2 md:mt-16 mt-8 font-cab leading-relaxed text-lg 2xl:text-3xl text-gray-700">
               I am a web developer from Greece. I help small businesses and
               individuals by building fast and modern websites.
             </h2>
@@ -55,8 +50,13 @@ const HomePageHeader = () => {
                 Stay ahead with your very own website!
               </p>
               <Link href="/contact">
-                <button className="bg-gray-200 border border-gray-600 hover:border-gray-300 font-cab 2xl:py-4 py-2 focus:outline-none px-4 shadow-md hover:bg-gray-800 2xl:text-2xl 2xl:px-6 hover:text-gray-200 mt-4 2xl:mt-10 transition duration-200 rounded-sm relative z-10">
+                <button className="bg-gray-200 border border-gray-600 hover:border-gray-300 font-cab 2xl:py-4 py-2 focus:outline-none mr-2 px-4 shadow-md hover:bg-gray-800 2xl:text-2xl 2xl:px-6 hover:text-gray-200 mt-4 2xl:mt-10 transition duration-200 rounded-sm relative z-10">
                   Get a website.
+                </button>
+              </Link>
+              <Link href="/devs">
+                <button className="bg-gray-200 border border-gray-400 font-cab 2xl:py-2 py-1 focus:outline-none px-4 shadow-sm hover:bg-gray-600 2xl:text-2xl 2xl:px-6 hover:text-gray-200 mt-4 2xl:mt-10 transition duration-200 rounded-sm relative z-10">
+                  About me
                 </button>
               </Link>
             </div>
@@ -87,8 +87,8 @@ const HomePageHeader = () => {
       <div className="absolute bottom-0 right-48 2xl:block hidden">
         <Image
           src="/07.png"
-          width={550}
-          height={550}
+          width={500}
+          height={500}
           alt="tree of ideas header background photo"
         />
       </div>
